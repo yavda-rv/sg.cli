7e397eef2a38b96548165dc2c1c4f48455d4edf6
radhey <radhey.varshney@yavda.in>
2021-08-11 21:34:22 +0530

    minor changes

--- bin/commands/start.js
+++ bin/commands/start.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const path = require('path');
 const cp = require("child_process");
+const printer = require("../lib/printer");
 
 const app = express();
 
 async function start(port) {
-    const tsc = cp.spawn("npm", ["run", "build:tsc:watch"], { stdio: "inherit" });
-    const rollup = cp.spawn("npm", ["run", "build:bundle:watch"], {stdio:"inherit"});
-
+    spwan("COMPILE", "npm", ["run", "build:tsc:watch"]);
+    spwan("BUNDLE ", "npm", ["run", "build:bundle:watch"]);
     runserver(port);
+}
+
+function spwan(tag, command, args) {
+    const child = cp.spawn(command, args, { stdio:"inherit"});
+    child.on("exit", (code, singal) => {
+        console.log(`${tag}: existed with code ${code} and signal ${singal}`);
+    });
 }
 
 function runserver(port) {
     const libPath = path.join(process.cwd(), "lib");
     app.use(express.static(libPath));
     app.get("/", function (req, res) {
-        return res.end("Superglue server running and serving " + process.cwd());
+        return res.end("Superglue server running and serving path: " + libPath);
     });
 
-    console.log('here');
     app.listen(port, () => {
-        console.log("server running at port: " + port);
+        printer.success(`SERVER : running at http://localhost:${port}/`);
     });
 }
 
