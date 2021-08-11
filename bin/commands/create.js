25092cd9e23b306e2b093c68c4885c67bfcec39b
radhey <radhey.varshney@yavda.in>
2021-08-11 19:15:20 +0530

    reading from template

--- bin/commands/create.js
+++ bin/commands/create.js
@@ -5,30 +5,28 @@ const writers = require("../core/writers");
 const spinner = require("../lib/spinner");
 const npm = require("../lib/npm");
 const git = require("../lib/git");
+const os = require("os");
+const treeify = require("treeify")
 
 async function create(name) {
     createDirectory(name);
-    createDirectory(path.join(name, 'src'));
 
     printer.info("Writing plugin files...");
-    await writers.superglueJson(name);
-    await writers.packageJson(name);
-    await writers.apiExtractorJson(name);
-    await writers.tsConfigJson(name);
-    await writers.rollupConfigJs(name);
-    await writers.gitIgnore(name);
-    await writers.entryPoint(name);
-    printer.info("Plugin files are generated");
+    let context = { name: name, user: os.userInfo().username };
+    let result = {};
+    await writers.copyTemplate(path.join(__dirname, "../core/templates"), name, context, result);
+    printer.info("Generated files: ");
+    console.log(treeify.asTree(result));
 
-    process.chdir(name);
-    
-    await spinner.spin("Initalizing git...", gitInit(process.cwd()));
-    printer.info("Git initialized");
+    // process.chdir(name);
 
-    await spinner.spin("Installing dependencies...", npmInit());
-    printer.info("Dependencies installed");
+    // await spinner.spin("Initalizing git...", gitInit(process.cwd()));
+    // printer.info("Git initialized");
 
-    printer.success("Plugin creation successful.");
+    // await spinner.spin("Installing dependencies...", npmInit());
+    // printer.info("Dependencies installed");
+
+    // printer.success("Plugin creation successful.");
 }
 
 async function gitInit(cwd) {
@@ -42,21 +40,21 @@ async function npmInit() {
 }
 
 function createDirectory(name) {
-    if(!fs.existsSync(name)) {
+    if (!fs.existsSync(name)) {
         fs.mkdirSync(name);
         return;
     }
 
-    if(!fs.existsSync(name)) {
+    if (!fs.existsSync(name)) {
         printer.error(`${name} is not valid. Please choose another name.`)
         process.exit(3);
     }
 
     let stat = fs.lstatSync(name);
-    if(stat.isDirectory() && fs.readdirSync(name).length == 0) // the directory is empty. OK for us.
+    if (stat.isDirectory() && fs.readdirSync(name).length == 0) // the directory is empty. OK for us.
         return;
 
-    if(stat.isFile()) {
+    if (stat.isFile()) {
         printer.error(`'${name} file already exists. Please choose another name.'`);
         process.exit(4);
     }
--- bin/core/writers.js
+++ bin/core/writers.js
@@ -1,49 +1,32 @@
 const fs = require('fs');
 const path = require('path');
-const os = require("os");
 const ejs = require("ejs");
 
-async function packageJson(name) {
-    let content = await ejs.renderFile(path.join(__dirname, "templates/package.json"), {name: name, user: os.userInfo().username});
-    fs.writeFileSync(path.join(name, "package.json"), content, { encoding: "utf-8" });
+async function writeTemplateFile(templateFileName, fileName, context) {
+    let content = await ejs.renderFile(templateFileName, context);
+    fs.writeFileSync(fileName, content, { encoding: "utf-8" });
 }
 
-async function apiExtractorJson(name) {
-    let content = await ejs.renderFile(path.join(__dirname, "templates/api-extractor.json"), {});
-    fs.writeFileSync(path.join(name, "api-extractor.json"), content, { encoding: "utf-8" });
-}
-
-async function tsConfigJson(name) {
-    let content = await ejs.renderFile(path.join(__dirname, "templates/tsconfig.client.json"), {});
-    fs.writeFileSync(path.join(name, "tsconfig.client.json"), content, { encoding: "utf-8" });
-}
-
-async function rollupConfigJs(name) {
-    let content = await ejs.renderFile(path.join(__dirname, "templates/rollup.config.js"), {});
-    fs.writeFileSync(path.join(name, "rollup.config.js"), content, { encoding: "utf-8" });
-}
-
-async function gitIgnore(name) {
-    let content = await ejs.renderFile(path.join(__dirname, "templates/.gitignore"), {});
-    fs.writeFileSync(path.join(name, ".gitignore"), content, { encoding: "utf-8" });
-}
-
-async function superglueJson(name) {
-    let content = await ejs.renderFile(path.join(__dirname, "templates/superglue.json"), {});
-    fs.writeFileSync(path.join(name, "superglue.json"), content, { encoding: "utf-8" });
-}
-
-async function entryPoint(name) {
-    let content = await ejs.renderFile(path.join(__dirname, "templates/src/EntryPoint.ts"), {});
-    fs.writeFileSync(path.join(name, "src", "EntryPoint.ts"), content, { encoding: "utf-8" });
+async function copyTemplate(baseDir, targetDir, context, result) {
+    let files = fs.readdirSync(baseDir);
+    for (let index = 0; index < files.length; index++) {
+        let file = files[index];
+        let filePath = path.join(baseDir, file);
+        if (fs.lstatSync(filePath).isDirectory()) {
+            let targetPath = path.join(targetDir, file);
+            if (!fs.existsSync(targetPath)) {
+                fs.mkdirSync(targetPath);
+                result[targetPath] = {}
+            }
+            await copyTemplate(filePath, targetPath, context, result[targetPath]);
+        } else {
+            let targetFileName = path.join(targetDir, file);
+            await writeTemplateFile(filePath, targetFileName, context);
+            result[targetFileName] = null;
+        }
+    }
 }
 
 module.exports = {
-    superglueJson,
-    packageJson,
-    apiExtractorJson,
-    tsConfigJson,
-    rollupConfigJs,
-    gitIgnore,
-    entryPoint
+    copyTemplate
 }
